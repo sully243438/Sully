805241d069c7cf55da9b71db08a92ed1c61a1ab4
Ahmed <S.Ahmed2474@student.leedsbeckett.ac.uk>
2024-10-17 16:03:07 +0100

    Adding work

---
+++ week 4.py
@@ -0,0 +1,12 @@
+import math
+result = math.sin (6.2)
+from math import sqrt
+root = sqrt (49)
+val = print(cos(radians(45)))
+from math import pi
+print ("the value of pi is", pi)
+
+def diplayTwice (msg)
+    print (msg)
+    print (msg)
+
